1e1cfe6cc9418e1a1e08ce549749a7c739deaa3b
Malek T <malek_tatin@yahoo.com>
2024-11-11 12:41:12 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items=[];
+    this.length = 0 ;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length === 0) {this.items.push(item);}
+    else {
+      for (let i=0 ; i < this.length ; i++){
+        if(this.items[i]>= item) {this.items.splice(i, 0, item);}
+      }
+    }
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items.at(-1);
+  }
 
-  min() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.items[0];
+  }
 
-  sum() {}
+  sum() {
+    return this.items.reduce((accu, value) => accu + value, 0);
+  }
 
-  avg() {}
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
